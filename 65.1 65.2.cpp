2f0acf400ea9ddf82f90f8e5fb9a6774a55f16ca
JuliaSzczygiel <62343530+JuliaSzczygiel@users.noreply.github.com>
2021-06-16 08:43:32 +0200

    Add files via upload

---
+++ 65.1 65.2.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include<fstream>
+using namespace std;
+
+int nwd(int a, int b){
+	if(a < b){
+		return nwd(b, a);
+	}
+	if(b==0){
+		return a;
+	}
+	return nwd(b, a%b);
+}
+
+class Ulamek {
+	public:
+	int licznik;
+	int mianownik;
+	double wartosc(){
+		return licznik/(double)mianownik;
+	};
+	int getNWD(){
+		return nwd(licznik, mianownik);
+	};
+};
+
+int main(){
+	ifstream dane("dane_ulamki.txt");
+	ofstream wyniki("wyniki_ulamki.txt");
+	int liczba = 0;
+	int inlineCount = 0;
+	
+	int licznik = 0;
+	int mianownik = 0;
+	
+	Ulamek minUlamek;
+	
+	int liczNieskracalne = 0;
+	int sumaLicznikow = 0;
+	long long sumaNowychLicznikow = 0;
+	
+	while(dane >> liczba){
+		//cout << liczba << endl;
+		
+		if(inlineCount == 0){
+			licznik = liczba;
+		} else if(inlineCount == 1){
+			mianownik = liczba;
+			
+			Ulamek ulamek;
+			ulamek.licznik = licznik;
+			ulamek.mianownik = mianownik;
+		
+			//podpunkt 1
+			if(ulamek.wartosc() < minUlamek.wartosc()){
+				minUlamek = ulamek;
+			} else if(ulamek.wartosc() == minUlamek.wartosc() && ulamek.mianownik < minUlamek.mianownik){
+				minUlamek = ulamek;
+			}
+			
+			//podpunkt 2
+			if(ulamek.getNWD() == 1){
+				liczNieskracalne++;
+			}
+			
+		}
+		
+		inlineCount++;
+		if(inlineCount >= 2){
+			inlineCount = 0;
+		}
+	}
+	
+	cout << "65.1. MinUlamek: " << minUlamek.licznik << " " << minUlamek.mianownik << endl;
+	cout << "65.2. LiczNieskracalne " << liczNieskracalne << endl;
+	
+	wyniki << "65.1. MinUlamek: " << minUlamek.licznik << " " << minUlamek.mianownik << endl;
+	wyniki << "65.2. LiczNieskracalne " << liczNieskracalne << endl;
+	
+	/*cout << nwd(24, 10) << endl;
+	cout << nwd(44, 4) << endl;
+	cout << nwd(3, 7) << endl;*/
+}
